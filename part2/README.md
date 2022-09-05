a6c10d6127a09255f15612d8dd18db0936fdd02b
Saul Hdz Lara <knighthdz97@gmail.com>
2022-09-04 20:20:21 -0500

    📦 NEW: Add part 2 exercise 2.1

---
+++ part2/README.md
@@ -0,0 +1,12 @@
+# Part 2: Communicating with server
+
+In this part, we'll take a look at how to render a data collection, how a user can submit data to a React application using HTML forms, how JavaScript code in the browser can fetch and handle data stored in a remote backend server and we'll take a quick look at a few simple ways of adding CSS styles to our React applications.
+
+## Exercises 2.1 - 2.20
+
+### Part 2a
+- [X] 2.1: Course information, step 6
+- [ ] 2.2: course information, step 7
+- [ ] 2.3: course information, step 8
+- [ ] 2.4: course information, step 9
+- [ ] 2.5: Separate module
---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,69 @@
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Part = ({ part }) => {
+  console.log(part)
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
